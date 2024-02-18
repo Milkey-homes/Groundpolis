308118f0265211997339b6d82c78ee3849d17faa
atsu1125 <atsuchan@atsuchan.page>
2024-02-18 19:24:53 +0900

    FILE_TYPE_WHITELIST to FILE_TYPE_BROWSERSAFE

--- src/const.ts
+++ src/const.ts
@@ -1,7 +1,8 @@
 // ブラウザで直接表示することを許可するファイルの種類のリスト
 // ここに含まれないものは application/octet-stream としてレスポンスされる
 // SVGはXSSを生むので許可しない
-export const FILE_TYPE_WHITELIST = [
+export const FILE_TYPE_BROWSERSAFE = [
+	// Images
 	'image/png',
 	'image/gif',
 	'image/jpeg',
@@ -10,21 +11,34 @@ export const FILE_TYPE_WHITELIST = [
 	'image/bmp',
 	'image/tiff',
 	'image/x-icon',
-	'video/mpeg',
-	'video/mp4',
-	'video/mp2t',
-	'video/webm',
+
+	// OggS
+	'audio/opus',
 	'video/ogg',
-	'video/3gpp',
-	'video/quicktime',
-	'video/x-m4v',
-	'video/x-msvideo',
-	'audio/mpeg',
-	'audio/aac',
-	'audio/wav',
-	'audio/webm',
 	'audio/ogg',
-	'audio/x-m4a',
-	'audio/x-flac',
 	'application/ogg',
+
+	// ISO/IEC base media file format
+	'video/quicktime',
+	'video/mp4',
+	'audio/mp4',
+	'video/x-m4v',
+	'audio/x-m4a',
+	'video/3gpp',
+	'video/3gpp2',
+
+	'video/mpeg',
+	'audio/mpeg',
+
+	'video/webm',
+	'audio/webm',
+
+	'audio/aac',
+	'audio/x-flac',
+	'audio/vnd.wave',
 ];
+/*
+https://github.com/sindresorhus/file-type/blob/main/supported.js
+https://github.com/sindresorhus/file-type/blob/main/core.js
+https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Containers
+*/
