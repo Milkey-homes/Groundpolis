4c3202574f43a48961fce37506dd5ed4ec6910cf
atsu1125 <atsuchan@atsuchan.page>
2024-02-23 15:27:23 +0900

    option to prefer softwareColor on ticker

--- src/client/store.ts
+++ src/client/store.ts
@@ -393,6 +393,11 @@ export const defaultStore = markRaw(new Storage('base', {
 		default: false
 	},
 
+	preferTickerSoftwareColor: {
+		where: 'device',
+		default: false
+	},
+
 	//#endregion
 }));
 
